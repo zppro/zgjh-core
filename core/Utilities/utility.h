886e06857fcca6afd7270b4f6126fbb68f762000
zppro <zpprozppro@gmail.com>
2012-05-24 14:31:01 +0800

    3rd

--- core/Utilities/utility.h
+++ core/Utilities/utility.h
@@ -1,15 +1,14 @@
 //
-//  core.h
+//  utility.h
 //  core
 //
 //  Created by 钟 平 on 12-5-24.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef core_core_h
-#define core_core_h
+#ifndef core_utility_h
+#define core_utility_h
 
-#import "catalog.h"
-#import "utility.h"
+#import "MacroFunctions.h"
 
 #endif
--- core/catalog.h
+++ core/catalog.h
@@ -1,15 +1,14 @@
 //
-//  core.h
+//  catalog.h
 //  core
 //
 //  Created by 钟 平 on 12-5-24.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef core_core_h
-#define core_core_h
+#ifndef core_catalog_h
+#define core_catalog_h
 
-#import "catalog.h"
-#import "utility.h"
+#import "CatalogOfUIView.h"
 
 #endif
